50d266e285ae298d241adb815e457754e8da60c3
Kevin Harrington <mad.hephaestus@gmail.com>
2020-07-21 18:47:45 -0400

    updating the doxygen

--- search/variables_10.js
+++ search/variables_10.js
@@ -5,7 +5,6 @@ var searchData=
   ['ticks',['ticks',['../classServo.html#a63c3d219b32ae9910c41ad9346e9ae8e',1,'Servo']]],
   ['timeoflastconnect',['timeOfLastConnect',['../classWifiManager.html#a5da30fe0eefad4cba2d39dd8a9852b5b',1,'WifiManager']]],
   ['timeoflastdisconnect',['timeOfLastDisconnect',['../classWifiManager.html#af05d6040edefb31ffa957265613200e4',1,'WifiManager']]],
-  ['timer',['timer',['../classRangefinder.html#a13b45c596fc0a29e2c87b075f5f11432',1,'Rangefinder']]],
   ['timer_5fwidth',['timer_width',['../classServo.html#a8901b6f01ee2f3b946a8730431198835',1,'Servo']]],
   ['timer_5fwidth_5fticks',['timer_width_ticks',['../classServo.html#aa68ae7b20692b1d48ca30e5a86392751',1,'Servo']]],
   ['timercount',['timerCount',['../classESP32PWM.html#a21b8a9c0ff46141f2960f916d32ff8d7',1,'ESP32PWM']]],
