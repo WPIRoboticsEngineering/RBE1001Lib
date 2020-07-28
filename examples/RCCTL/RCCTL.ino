0f2e755dfd28d9714876b68d2530f5fcfc3bf2e5
Alex Camilo <alex.camilo@gmail.com>
2020-07-28 19:32:20 -0400

    Modified example to include web code.

--- examples/RCCTL/RCCTL.ino
+++ examples/RCCTL/RCCTL.ino
@@ -1,72 +1,108 @@
-#include <Arduino.h>
-#include <RBE1001Lib.h>
-#include "Motor.h"
-#include "Rangefinder.h"
-#include <ESP32Servo.h>
 #include <ESP32AnalogRead.h>
-// https://wpiroboticsengineering.github.io/RBE1001Lib/classMotor.html
-Motor motor1;
-Motor motor2;
-// https://wpiroboticsengineering.github.io/RBE1001Lib/classRangefinder.html
-Rangefinder rangefinder1;
-// https://wpiroboticsengineering.github.io/RBE1001Lib/classServo.html
-Servo lifter;
-// https://wpiroboticsengineering.github.io/RBE1001Lib/classESP32AnalogRead.html
-ESP32AnalogRead leftLineSensor;
-ESP32AnalogRead rightLineSensor;
-ESP32AnalogRead servoPositionFeedback;
-bool upDown=false;
+#include <Esp32WifiManager.h>
+#include "DriveBase.h"
+#include "SimpleWebServer.h"
+#include "WebPage.h"
+#include "Rangefinder.h"
+#include "LineTrackSensor.h"
+#include "Timer.h"
+#include "RBE1001Lib.h"
+
+
+//Rangefinder rangefinder(FORWARD_ULTRASONIC_TRIG, FORWARD_ULTRASONIC_ECHO);
+LineTrackSensor lineTrackSensor(LEFT_LINE_SENSE, RIGHT_LINE_SENSE);
+DriveBase drive(MOTOR1_PWM, MOTOR2_PWM,MOTOR1_DIR,MOTOR2_DIR);
+SimpleWebServer webServer;
+WebPage buttonPage(webServer);
+
+enum State {stopped, go} state;
+
+/*
+ * Each of these functions corresponds to a button on the web page. When the
+ * associated button is pressed, the function is called. The functions can
+ * contain any robot code, but in this case, I just set a new state and the
+ * state machine in the loop() function starts doing whatever task you
+ * selected
+ */
+void startMotor() {
+  state = go;
+  buttonPage.sendHTML();
+}
+
+/*
+ * Set up all the buttons that you will use for your final project
+ * Each "newButton" call adds a button and connects the button to a function
+ * that you add to the program like stopMotor()
+ * that you can see above. At the end of each of those functions you need to
+ * call "buttonPage.sendHTML()" to force the web page to redisplay after the
+ * button is pressed.
+ */
+void setupButtons() {
+  buttonPage.newButton("on", startMotor, "Motors On", "Turn On the motors");
+}
+
+Timer endTimer;   // end of a timed state timer
+Timer dashboardUpdateTimer;  // times when the dashboard should update
+
 /*
  * This is the standard setup function that is called when the ESP32 is rebooted
  * It is used to initialize anything that needs to be done once.
+ * In this example, it sets the Serial Console speed, initializes the web server,
+ * sets up some web page buttons, resets some timers, and sets the initial state
+ * the robot should start in
  */
 void setup() {
-  // This will initialize the Serial as 115200 for prints
-  Serial.begin(115200);
-  Motor::allocateTimer(0); // used by the DC Motors
-  ESP32PWM::allocateTimer(1);// Used by servos
-  // pin definitions https://wpiroboticsengineering.github.io/RBE1001Lib/RBE1001Lib_8h.html#define-members
-  motor2					.attach(MOTOR2_PWM, MOTOR2_DIR, MOTOR2_ENCA, MOTOR2_ENCB);
-  motor1					.attach(MOTOR1_PWM, MOTOR1_DIR, MOTOR1_ENCA, MOTOR1_ENCB);
-  rangefinder1				.attach(SIDE_ULTRASONIC_TRIG, SIDE_ULTRASONIC_ECHO);
-  lifter					.attach(SERVO_PIN);
-  leftLineSensor			.attach(LEFT_LINE_SENSE);
-  rightLineSensor			.attach(RIGHT_LINE_SENSE);
-  servoPositionFeedback		.attach(SERVO_FEEDBACK_SENSOR);
-
+  // This will initialize the Serial as 115200 for prints and passwords
+  webServer.initialize();
+  setupButtons();
+  buttonPage.finishPage();  // this adds the html to the page after the buttons
+  endTimer.reset();     // reset the end of state timer
+  dashboardUpdateTimer.reset(); // reset the dashbaord refresh timer
+  state = stopped;   // initially, the robot is in the stopped state
 }
 
+/*
+ * this is the state machine.
+ * You can add additional states as desired. The switch statement will execute
+ * the correct code depending on the state the robot is in currently.
+ * For states that require timing, like turning and straight, they use a timer
+ * that is zeroed when the state begins. It is compared with the number of
+ * milliseconds the robot should reamain in that state.
+ */
+void runStateMachine() {
+  switch(state) {
+    case stopped:
+      drive.motors(0, 0);
+      break;
+    case go:
+      drive.motors(70, 70);
+      break;
+    }
+}
+
+/*
+ * This function updates all the dashboard status that you would like to see
+ * displayed periodically on the web page. You are free to add more values
+ * to be displayed to help debug your robot program by calling the
+ * "setValue" function with a name and a value.
+ */
+void updateDashboard() {
+    // This writes values to the dashboard area at the bottom of the web page
+    if (dashboardUpdateTimer.getMS() > 500) {
+      //buttonPage.setValue("Rangefinder", rangefinder.getDistanceCM());
+      buttonPage.setValue("Left linetracker", lineTrackSensor.getLeft());
+      buttonPage.setValue("Right linetracker", lineTrackSensor.getRight());
+      dashboardUpdateTimer.reset();
+    }
+}
 
 /*
  * The main loop for the program. The loop function is repeatedly called
- * once the ESP32 is started.
+ * once the ESP32 is started. In here we run the state machine, update the
+ * dashboard data, and handle any web server requests.
  */
 void loop() {
-	upDown=!upDown;
-	int loopTime = 4000;// 4 second loop
-	int servoRange =180;
-
-	motor2.SetSetpointWithSinusoidalInterpolation(upDown?360:0, loopTime);
-	motor1.SetSetpointWithLinearInterpolation(upDown?360:0, loopTime);
-
-	for(int i=0;i<servoRange;i++){
-		delay(loopTime/servoRange);
-		Serial.println("\n");
-		Serial.print("Range 1 "+String(rangefinder1.getDistanceCM()));
-//		Serial.print("Speed 1 "+String(motor1.getDegreesPerSecond())+
-//					" Speed 2 "+String(motor2.getDegreesPerSecond()));
-		Serial.print("\tCount 1 "+String(motor1.getCurrentDegrees())+
-						"\t Count 2 "+String(motor2.getCurrentDegrees()));
-		Serial.print("\t Line left "+String(leftLineSensor.readVoltage()));
-
-		Serial.print(	"\t Line right "+String(rightLineSensor.readVoltage()));
-		Serial.print("\t Servo Read "+String(servoPositionFeedback.readVoltage()));
-		Serial.print("\t Servo write "+String(i));
-		lifter.write(i);
-	}
-
-
-
-
+    runStateMachine();  // do a pass through the state machine
+    updateDashboard();  // update the dashboard values
+    webServer.handleClient();  // handle web page requests
  }
-
