676c67bc52bc02c14099e6b58c9324c6b6e0716b
Alex Camilo <alex.camilo@gmail.com>
2020-08-06 14:32:19 -0400

    Merged, wired in servo.

--- src/WebPage.h
+++ src/WebPage.h
@@ -16,6 +16,7 @@ class WebPage {
 
     void setSliderValue(uint32_t number, float value);
     void setJoystickValue(float xpos, float ypos, float angle, float mag);
+    void setValue(String value, float data);
     float sliders[4];
     JoyData joystick;
 
