093c5e37f7fff93738697baa1fa010b72c98189e
Alex Camilo <alex.camilo@gmail.com>
2020-08-20 23:55:31 -0400

    Update WebPage.cpp

--- src/WebPage.cpp
+++ src/WebPage.cpp
@@ -164,8 +164,8 @@ void updateTask(void *param){
 
 		lock();
 		//Serial.println("L data Lock");
-		if(thisPage->sendHeartbeat()) continue;
-		if (thisPage->SendAllLabels()) continue;
+		thisPage->sendHeartbeat();
+		thisPage->SendAllLabels();
 		thisPage->SendAllValues();
 		unlock();
 
