85e9cc605b9d28a7983d39b58bd6937db222ea4f
Kevin Harrington <mad.hephaestus@gmail.com>
2020-08-27 14:31:23 -0400

    Comments on example

--- examples/MotorTest/MotorTest.ino
+++ examples/MotorTest/MotorTest.ino
@@ -11,6 +11,7 @@ bool upDown=false;
 void setup() {
   // This will initialize the Serial as 115200 for prints
   Serial.begin(115200);
+  // Set up the motors Timer for use in making PWM's
   Motor::allocateTimer(0);
   // pin definitions https://wpiroboticsengineering.github.io/RBE1001Lib/RBE1001Lib_8h.html#define-members
   left_motor.attach(MOTOR_LEFT_PWM, MOTOR_LEFT_DIR, MOTOR_LEFT_ENCA, MOTOR_LEFT_ENCB);
@@ -26,26 +27,18 @@ void loop() {
 	upDown=!upDown;
 	left_motor.moveTo(upDown?360*2.5:0, 360);
 	right_motor.moveTo(upDown?360*2.5:0, 360);
-	//left_motor.SetSetpointWithLinearInterpolation(upDown?3600:0, 8000);
-	//right_motor.SetSetpointWithLinearInterpolation(upDown?360:0, 2000);
-	//right_motor.SetSetpointWithBezierInterpolation(upDown?3600:0, 8000,0.45,1);
-	//right_motor.setSetpointWithTrapezoidalInterpolation(upDown?3600:0, 8000, 500);
-	double peak1 = 0;
-	double peak2 =0;
 
+	// Check both motors to see if they are don moving yet
 	while(!left_motor.isMotorDoneWithMove() &&
-			!right_motor.isMotorDoneWithMove()){
-		if(abs(left_motor.getDegreesPerSecond())>peak1){
-			peak1=abs(left_motor.getDegreesPerSecond());
-		}
-		if(abs(right_motor.getDegreesPerSecond())>peak2){
-			peak2=abs(right_motor.getDegreesPerSecond());
-		}
+			!right_motor.isMotorDoneWithMove())
+	{
+		// print the state of the motors while running
 		delay(20);
 		Serial.println("motor compared  "+String(right_motor.getInterpolationUnitIncrement()-left_motor.getInterpolationUnitIncrement())+
 				+" Interp "+String(right_motor.getInterpolationUnitIncrement())+
 				+" Vel 1 "+String(left_motor.getDegreesPerSecond())+" Vel 2 "+String(right_motor.getDegreesPerSecond()));
 	}
+	// Print the count at the end of a loop
 	delay(100);
 	Serial.println("Count 1 "+String(left_motor.getCurrentDegrees())+
 					" Count 2 "+String(right_motor.getCurrentDegrees()));
