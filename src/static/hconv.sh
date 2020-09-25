4903d54bdd9c2cf4e9d6a1bc1a1305a75d28757d
Alex Camilo <alex.camilo@gmail.com>
2020-09-25 19:14:31 -0400

    Better minetype guessing

--- src/static/hconv.sh
+++ src/static/hconv.sh
@@ -4,8 +4,9 @@
 read -r -d '' MANIFEST_STRUCT << EOM
 typedef struct
 {
-  const char * filename;
-  const char * filedata;
+  const char * name;
+  const char * data;
+  const char * mime;
   int length;
 } PACKED_FILE;
 
@@ -20,7 +21,7 @@ static const PACKED_FILE static_files_manifest[] =
 EOM
 
 read -r -d '' MANIFEST_POST << EOM
-  {NULL,NULL,NULL}
+
 };
 EOM
 
@@ -28,14 +29,14 @@ EOM
 echo "$MANIFEST_STRUCT" > static.h
 
 #Collect up our static documents
-find . -type f -not -name "*.h" -not -name "*.sh"  -print0 | while read -r -d '' file_name
+find . -type f -not -name "*.h" -not -name "*.sh" -not -name "*.magic" -print0 | while read -r -d '' file_name
 do
   echo "Packing $file_name"
   #Convert to C array and append to packed file.
   #Make sure files terminate with a 0 so they're valud strings.
   #Make sure the array type is static const.
   #Append to static files
-  xxd -i $file_name | sed --expression 's/unsigned/static const/g' | cat >> static.h
+  xxd -i $file_name | sed --expression 's/unsigned/static const/g' | sed --expression 's/};/,0x00};/g' | cat >> static.h
 done
 
 # Build manifest
@@ -47,12 +48,37 @@ echo >> static.h
 FILE_COUNT=`cat static.h | grep "static const char __" | awk '{print $4}' | sed "s/\[\]//" | wc -l`
 
 printf "static const int static_files_manifest_count = %s;\n" "$FILE_COUNT" >> static.h
+
+
 #const strings for filenames.
 for f in `cat static.h | grep "static const char __" | awk '{print $4}' | sed "s/\[\]//"`
 do
   FNAME=`echo $f | sed "s/__//" | sed "s/_/./g" | cat`
   CNAME=$f
-  printf "static const char _filename%s[] = \"%s\"; \n" "$CNAME" "$FNAME" >>static.h
+  printf "static const char _filename%s[] = \"/%s\"; \n" "$CNAME" "$FNAME" >>static.h
+done
+
+#const strings for file MIME.
+
+echo >> static.h
+echo >> static.h
+echo "// MIME Strings" >> static.h
+echo >> static.h
+
+
+# Thank You https://github.com/cweiske/MIME_Type_PlainDetect for the awesome magic file.
+for f in `cat static.h | grep "static const char __" | awk '{print $4}' | sed "s/\[\]//"`
+do
+  FNAME=`echo $f | sed "s/__//" | sed "s/_/./g" | cat`
+  CNAME=$f
+  case "$(echo $FNAME | awk -F. '{print $NF}' )" in
+    html) FMIME="text/html" ;;
+    js)   FMIME="text/javascript" ;;
+    css)   FMIME="text/css" ;;
+    *) FMIME="$(file -m programming.magic --mime-type $FNAME | cut -d ' ' -f2)"
+  esac
+  echo $FMIME
+  printf "static const char _MIME%s[] = \"%s\"; \n" "$CNAME" "$FMIME" >>static.h
 done
 
 
@@ -66,7 +92,7 @@ for f in `cat static.h | grep "static const char __" | awk '{print $4}' | sed "s
 do
 
   CNAME=$f
-  printf "\t{_filename%s,\t %s,\t %s_len}, \n" "$CNAME" "$CNAME" "$CNAME"  >>static.h
+  printf "\t{_filename%s,\t %s,\t_MIME%s,\t%s_len}, \n" "$CNAME" "$CNAME" "$CNAME" "$CNAME"  >>static.h
 done
 
 echo "$MANIFEST_POST" >> static.h
